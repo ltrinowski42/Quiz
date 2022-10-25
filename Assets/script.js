eecde4d55ba2ab3fc17907ac0c313d2b9698e966
ltrinowski <ltrinowski42@gmail.com>
2022-10-24 20:42:14 -0400

    starting adding vars

---
+++ Assets/script.js
@@ -0,0 +1,30 @@
+var header = document.querySelector(".header");
+
+var quizQuestionsPage = document.getElementById("quizPage");
+var quizQuestionHeader = document.getElementById("quizHeader");
+var quizChallengePage = document.getElementById("questionsPage");
+var finalScorePage = document.getElementById("finalScorePage");
+
+var choice1 = document.getElementById("one");
+var choice2 = document.getElementById("two");
+var choice3 = document.getElementById("three");
+var choice4 = document.getElementById("four");
+
+var correct = document.getElementById("correct");
+var answerResponse = document.getElementById("answer");
+var score = document.getElementById("score");
+var finalScoreIs = document.getElementById("finalScore");
+
+var questionButton = document.querySelector(".questionButton");
+var submitButton = document.getElementById("submitButton");
+var highScoreButtons = document.getElementById("highScoreButtons");
+
+var initials = document.getElementById("initials"); 
+var initialButton = document.getElementById("initialButton"); 
+var initialInput = document.getElementById("initialInput"); 
+
+var allDone = document.getElementById("finished");
+var allDoneButtons = document.getElementById("form-inline");
+
+var timer = document.getElementById("timer");
+
