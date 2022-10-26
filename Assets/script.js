348a5b113a0d2a0035930f0918c5b18ebac740bd
ltrinowski <ltrinowski42@gmail.com>
2022-10-25 21:56:12 -0400

    started adding array and functions

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,37 @@ var goBackButton = document.querySelector("#go-back");
 var clearScoresButton = document.querySelector("#clear-high-scores");
 var correctEl = document.getElementById("correct");
 var wrongEl = document.getElementById("wrong");
+
+var answerArray = [
+    ['Commonly used data types DO NOT include:','strings','booleans','alerts','numbers','3'],
+    ['The condition in an if / else statement is enclosed within ______.', 'quotes', 'curly braces', 'parentheses', 'square brackets', '3'],
+    ['Arrays in JavaScript can be used to store ______.', 'numbers and strings', 'other arrays', 'booleans', 'all of the above', '4'],
+    ['String values must be enclosed with _______ when being assinged to variables', 'commas', 'curly braces', 'quotes', 'parentheses', '3'],
+    ['A very useful tool used during development and debugging for printing content to the debugger is:', 'JavaScript', 'terminal/bash', 'for loops', 'console.log', '4']
+];
+
+var QuestionIndex = 0
+
+let renderStartPage = function () {
+    highScorePageContainer.classList.add("hide")
+    highScorePageContainer.classList.remove("show")
+    startingPageContainer.classList.remove("hide")
+    startingPageContainer.classList.add("show")
+    scoreContainer.removeChild(scoreContainer.lastChild)
+    QuestionIndex = 0
+    gameover = ""
+    timerEl.textContent = 0 
+    score = 0
+
+    if (correctEl.className = "show") {
+        correctEl.classList.remove("show");
+        correctEl.classList.add("hide")
+    }
+    if (wrongEl.className = "show") {
+        wrongEl.classList.remove("show");
+        wrongEl.classList.add("hide");
+    }
+}
+
+
+
